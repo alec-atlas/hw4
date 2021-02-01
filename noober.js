7c78516ecc957f45d5ea866b6874d31960f9be82
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-02-01 16:20:30 -0600

    changed second loop

--- noober.js
+++ noober.js
@@ -20,7 +20,8 @@ async function pageLoaded() {
                 levelOfService = 'Noober XL'
               } else {
                 levelOfService = 'Noober X'
-              }      
+              } 
+                console.log(levelOfService)
                 passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
                 passenger1Phone = ride[0].passengerDetails.phoneNumber
                 passenger1NumberOfPassengers = ride[0].numberOfPassengers
@@ -28,6 +29,13 @@ async function pageLoaded() {
                 passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
                 passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
                 passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
+                console.log(passenger1Name)
+                console.log(passenger1Phone)
+                console.log(passenger1NumberOfPassengers)
+                console.log(passenger1PickupAddressLine1)
+                console.log(passenger1PickupAddressLine2)
+                console.log(passenger1DropoffAddressLine1)
+                console.log(passenger1DropoffAddressLine2)
               
                 if (ride.length > 1) {
                   passenger2Name = `${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}`
@@ -37,6 +45,13 @@ async function pageLoaded() {
                   passenger2PickupAddressLine2 = `${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
                   passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
                   passenger2DropoffAddressLine2 = `${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}`
+                  console.log(passenger2Name)
+                  console.log(passenger2Phone)
+                  console.log(passenger2NumberOfPassengers)
+                  console.log(passenger2PickupAddressLine1)
+                  console.log(passenger2PickupAddressLine2)
+                  console.log(passenger2DropoffAddressLine1)
+                  console.log(passenger2DropoffAddressLine2)
                   }
                   if (ride.length > 2) {
                     passenger3Name = `${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}`
@@ -46,6 +61,13 @@ async function pageLoaded() {
                     passenger3PickupAddressLine2 = `${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
                     passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
                     passenger3DropoffAddressLine2 = `${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}`
+                    console.log(passenger3Name)
+                    console.log(passenger3Phone)
+                    console.log(passenger3NumberOfPassengers)
+                    console.log(passenger3PickupAddressLine1)
+                    console.log(passenger3PickupAddressLine2)
+                    console.log(passenger3DropoffAddressLine1)
+                    console.log(passenger3DropoffAddressLine2)
                     }
         }}
         
@@ -91,5 +113,5 @@ async function pageLoaded() {
             }
         }
       
-      }
+}
 window.addEventListener('DOMContentLoaded', pageLoaded)
