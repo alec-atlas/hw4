9f726494a564a87aa8b264c418cdc4eef5bdbebb
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-02-01 22:57:47 -0600

    latest and greatest

--- noober.js
+++ noober.js
@@ -23,7 +23,7 @@ async function pageLoaded() {
               } 
             } 
 
-            if (ride.length > 0){
+            if (ride.length = 1){
             let outputElement = document.querySelector('.rides')
             outputElement.insertAdjacentHTML('beforeend',`<h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
                   <i class="fas fa-car-side"></i>
@@ -58,59 +58,147 @@ async function pageLoaded() {
 
                 else if (ride.length = 2) {
                   let outputElement = document.querySelector('.rides')
-                  outputElement.insertAdjacentHTML('beforeend',`<div class="border-4 border-gray-900 p-4 my-4 text-left">
+                  outputElement.insertAdjacentHTML('beforeend',`<h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+                  <i class="fas fa-car-side"></i>
+                  <span>${levelOfService}</span>
+                </h1>
+            
+                <div class="border-4 border-gray-900 p-4 my-4 text-left">
                   <div class="flex">
                     <div class="w-1/2">
-                      <h2 class="text-2xl py-1">${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}</h2>
-                      <p class="font-bold text-gray-600">${ride[1].passengerDetails.phoneNumber}</p>
+                      <h2 class="text-2xl py-1">${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}</h2>
+                      <p class="font-bold text-gray-600">${ride[0].passengerDetails.phoneNumber}</p>
                     </div>
                     <div class="w-1/2 text-right">
                       <span class="rounded-xl bg-gray-600 text-white p-2">
-                      ${ride[1].numberOfPassengers} passengers
+                      ${ride[0].numberOfPassengers} passengers
                       </span>
                     </div>
                   </div>
                   <div class="mt-4 flex">
                     <div class="w-1/2">
                       <div class="text-sm font-bold text-gray-600">PICKUP</div>
-                      <p>${ride[1].pickupLocation.address}</p>
-                      <p>${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}</p>
+                      <p>${ride[0].pickupLocation.address}</p>
+                      <p>${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}</p>
                     </div>
                     <div class="w-1/2">
                       <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-                      <p>${ride[1].dropoffLocation.address}</p>
-                      <p>${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}</p>
+                      <p>${ride[0].dropoffLocation.address}</p>
+                      <p>${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}</p>
                     </div>
                   </div>
-                </div>`) }
-
-              else if (ride.length = 3) {
-                let outputElement = document.querySelector('.rides')
-                outputElement.insertAdjacentHTML('beforeend',`<div class="border-4 border-gray-900 p-4 my-4 text-left">
+                </div>
+                 
+                <div class="border-4 border-gray-900 p-4 my-4 text-left">
                 <div class="flex">
                   <div class="w-1/2">
-                    <h2 class="text-2xl py-1">${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}</h2>
-                    <p class="font-bold text-gray-600">${ride[2].passengerDetails.phoneNumber}</p>
+                    <h2 class="text-2xl py-1">${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}</h2>
+                    <p class="font-bold text-gray-600">${ride[1].passengerDetails.phoneNumber}</p>
                   </div>
                   <div class="w-1/2 text-right">
                     <span class="rounded-xl bg-gray-600 text-white p-2">
-                    ${ride[2].numberOfPassengers} passengers
+                    ${ride[1].numberOfPassengers} passengers
                     </span>
                   </div>
                 </div>
                 <div class="mt-4 flex">
                   <div class="w-1/2">
                     <div class="text-sm font-bold text-gray-600">PICKUP</div>
-                    <p>${ride[2].pickupLocation.address}</p>
-                    <p>${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}</p>
+                    <p>${ride[1].pickupLocation.address}</p>
+                    <p>${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}</p>
                   </div>
                   <div class="w-1/2">
                     <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-                    <p>${ride[2].dropoffLocation.address}</p>
-                    <p>${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}</p>
+                    <p>${ride[1].dropoffLocation.address}</p>
+                    <p>${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}</p>
                   </div>
                 </div>
-              </div>`) 
+              </div>`) }
+
+              else if (ride.length = 3) {
+                let outputElement = document.querySelector('.rides')
+                outputElement.insertAdjacentHTML('beforeend',`<h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+                <i class="fas fa-car-side"></i>
+                <span>${levelOfService}</span>
+              </h1>
+          
+              <div class="border-4 border-gray-900 p-4 my-4 text-left">
+                <div class="flex">
+                  <div class="w-1/2">
+                    <h2 class="text-2xl py-1">${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}</h2>
+                    <p class="font-bold text-gray-600">${ride[0].passengerDetails.phoneNumber}</p>
+                  </div>
+                  <div class="w-1/2 text-right">
+                    <span class="rounded-xl bg-gray-600 text-white p-2">
+                    ${ride[0].numberOfPassengers} passengers
+                    </span>
+                  </div>
+                </div>
+                <div class="mt-4 flex">
+                  <div class="w-1/2">
+                    <div class="text-sm font-bold text-gray-600">PICKUP</div>
+                    <p>${ride[0].pickupLocation.address}</p>
+                    <p>${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}</p>
+                  </div>
+                  <div class="w-1/2">
+                    <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+                    <p>${ride[0].dropoffLocation.address}</p>
+                    <p>${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}</p>
+                  </div>
+                </div>
+              </div>
+               
+              <div class="border-4 border-gray-900 p-4 my-4 text-left">
+              <div class="flex">
+                <div class="w-1/2">
+                  <h2 class="text-2xl py-1">${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}</h2>
+                  <p class="font-bold text-gray-600">${ride[1].passengerDetails.phoneNumber}</p>
+                </div>
+                <div class="w-1/2 text-right">
+                  <span class="rounded-xl bg-gray-600 text-white p-2">
+                  ${ride[1].numberOfPassengers} passengers
+                  </span>
+                </div>
+              </div>
+              <div class="mt-4 flex">
+                <div class="w-1/2">
+                  <div class="text-sm font-bold text-gray-600">PICKUP</div>
+                  <p>${ride[1].pickupLocation.address}</p>
+                  <p>${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}</p>
+                </div>
+                <div class="w-1/2">
+                  <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+                  <p>${ride[1].dropoffLocation.address}</p>
+                  <p>${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}</p>
+                </div>
+              </div>
+            </div>
+                     
+            <div class="border-4 border-gray-900 p-4 my-4 text-left">
+            <div class="flex">
+              <div class="w-1/2">
+                <h2 class="text-2xl py-1">${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}</h2>
+                <p class="font-bold text-gray-600">${ride[2].passengerDetails.phoneNumber}</p>
+              </div>
+              <div class="w-1/2 text-right">
+                <span class="rounded-xl bg-gray-600 text-white p-2">
+                ${ride[2].numberOfPassengers} passengers
+                </span>
+              </div>
+            </div>
+            <div class="mt-4 flex">
+              <div class="w-1/2">
+                <div class="text-sm font-bold text-gray-600">PICKUP</div>
+                <p>${ride[2].pickupLocation.address}</p>
+                <p>${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}</p>
+              </div>
+              <div class="w-1/2">
+                <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+                <p>${ride[2].dropoffLocation.address}</p>
+                <p>${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}</p>
+              </div>
+            </div>
+          </div>`) 
             }
               
           }
